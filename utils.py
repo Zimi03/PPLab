e57f2f99153c9d6603aa8dedcc20b86a7e606bea
Zimi03 <szymon@MacBook-Pro-Szymon.local>
2025-05-13 14:37:42 +0200

    utils refactor

--- utils.py
+++ utils.py
@@ -1,20 +1,21 @@
-'''Module with functions to operate on numbers'''
+"""Module with functions to operate on numbers"""
+
 
 def add(a: int, b: int) -> int:
-    '''Function to add two numbers'''
+    """Function to add two numbers"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    '''Function to subtract two numbers'''
+    """Function to subtract two numbers"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    '''Function to multiply two numbers'''
+    """Function to multiply two numbers"""
     return a * b
 
 
 def divide(a: int, b: int) -> int:
-    '''Function to divide two numbers'''
+    """Function to divide two numbers"""
     return a / b
