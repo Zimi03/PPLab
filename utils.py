761d50dc926bf792b5cab9a0395001150a55bab1
Zimi03 <szymon@MacBook-Pro-Szymon.local>
2025-05-13 14:34:24 +0200

    utils refactor

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 def add(a: int, b: int) -> int:
+    '''Function to add two numbers'''
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    '''Function to subtract two numbers'''
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    '''Function to multiply two numbers'''
     return a * b
 
 
 def divide(a: int, b: int) -> int:
+    '''Function to divide two numbers'''
     return a / b
