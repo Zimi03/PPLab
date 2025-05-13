9b7ac0f5038a728423df35f6a2f7931098982ec1
Zimi03 <szymon@MacBook-Pro-Szymon.local>
2025-05-13 14:04:36 +0200

    Function and test commit

---
+++ test_utils.py
@@ -0,0 +1,26 @@
+import pytest
+import utils
+
+@pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (2, 3, 5), (3, 4, 7)])
+
+def test_add(a, b, expected):
+	result = utils.add(a, b)
+	assert result == expected
+
+@pytest.mark.parametrize("a, b, expected", [(1, 2, -1), (2, 1, 1)])
+
+def test_subtract(a, b, expected):
+	result = util.subtract(a, b)
+	assert result == expected
+
+@pytest.mark.parametrize("a, b, expected", [(1, 2, 2), (2, 3, 6), (5, 3, 15)])
+
+def test_multiply(a, b, expected):
+	result = utils.multiply(a, b)
+	result == expected
+
+@pytest.mark.parametrize("a, b, expected",  [(2, 2, 1), (4, 2, 2), (6, 3, 2)])
+
+def test_divide(a, b, expected):
+	result = utils.divide(a, b)
+	assert result == expected
